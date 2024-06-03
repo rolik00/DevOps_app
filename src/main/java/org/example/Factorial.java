9de09a400dd3d621b4061947a475b745a1abf674
rolik00 <lelya_rodina_01@mail.ru>
2024-06-03 17:58:37 +0300

    update check.yaml

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -2,8 +2,10 @@ package org.example;
 
 import java.math.BigInteger;
 
-public class Factorial {
-    public static BigInteger getFactorial(int f) {
+public class Factorial
+{
+    public static BigInteger getFactorial(int f)
+    {
         if (f <= 1) return BigInteger.valueOf(1);
         else return BigInteger.valueOf(f).multiply(getFactorial(f - 1));
     }
