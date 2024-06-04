b69befa5e419749c0eda95d3f64f4fed312bfbab
rolik00 <lelya_rodina_01@mail.ru>
2024-06-04 20:21:11 +0300

    update check_formatting.yml

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -2,10 +2,8 @@ package org.example;
 
 import java.math.BigInteger;
 
-public class Factorial
-{
-    public static BigInteger getFactorial(int f)
-    {
+public class Factorial {
+    public static BigInteger getFactorial(int f) {
         if (f <= 1) return BigInteger.valueOf(1);
         else return BigInteger.valueOf(f).multiply(getFactorial(f - 1));
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
